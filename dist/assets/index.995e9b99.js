c8027d782e9cedf7a6a9b58fe40457740d0b83d1
Noah Hsu <i@nn.ci>
2025-01-28 06:15:28 +0000

    upload heads/main dist files

---
+++ dist/assets/index.995e9b99.js
@@ -0,0 +1,1 @@
+import{h as t,ac as e,a7 as r}from"./index.378d0000.js";import n from"./Upload.e550da50.js";import"./index.03881e7d.js";const m=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{m as default};
